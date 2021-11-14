367bdd5806cbe0f8869c5ffa89c3ede7755eddc4
Aaron Lee <aaronlab.net@gmail.com>
2021-11-14 23:27:17 +0900

    Hello World

---
+++ Hello World/Hello World/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  Hello World
+//
+//  Created by Aaron Lee on 2021/11/14.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * arvg[]) {
+    printf("Hello world!\n");
+    return 0;
+}
