e463d37f9dd2bb32e0216ac5658313c39ea15d78
Aaron Lee <aaronlab.net@gmail.com>
2021-11-17 23:21:03 +0900

    Init proj

---
+++ ConstantAndVariable/ConstantAndVariable/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ConstantAndVariable
+//
+//  Created by Aaron Lee on 2021/11/17.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
